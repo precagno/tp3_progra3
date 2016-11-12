3fba56c763155122f13348d3607dbc74d52ab769
Pablo <pablosrecagno88@gmail.com>
2016-11-12 13:25:27 -0300

    Método resolver de SolverGoloso: terminado.Falta testear

--- src/logica_negocio/SolverGoloso.java
+++ src/logica_negocio/SolverGoloso.java
@@ -1,22 +1,45 @@
 package logica_negocio;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+
+import modelo.DAOfertas;
 
 public class SolverGoloso implements Solver {//adiere a interface Solver
 	
 	//variables de instancia
 	private Comparator<Oferta> _comparador;
+	private String _nombreArchivo;
 	
 	//constructor
 	public SolverGoloso(Comparator<Oferta> comparador){
 		_comparador=comparador;
 	}
 
-	public Subconjunto resolver(Instancia instancia) {
+	public void setNombreArchivo(String nombre){
+		_nombreArchivo=nombre;
+	}
+	
+	public Subconjunto resolver() {
+		
 		Subconjunto subconjunto=new Subconjunto();
+		Instancia instancia=null;
+		
+		try{
+			
+			if(_nombreArchivo==null){
+				throw new Exception("El nombre del archivo de ofertas no fue seteado");
+			}
+			
+			instancia = creadorInstancia();
+		}catch(Exception excepcion){
+			System.out.println(excepcion.getMessage());
+		}
+		
 		ArrayList<Oferta> ofertasOrdenadas=this.ordenarOfertas(instancia);
+		
 		for (Oferta oferta : ofertasOrdenadas) {
 			
 			int duracionOferta=oferta.getDemandaHoraria().getTiempo();
@@ -30,7 +53,20 @@ public class SolverGoloso implements Solver {//adiere a interface Solver
 		return subconjunto;
 	}
 
-	//ordenamiento
+	/*-- Métodos auxiliares --*/
+	
+	/*-- Crea una instancia a partir de ofertas anteriormente persistidas --*/
+	private Instancia creadorInstancia() throws IOException{
+		Instancia aux=new Instancia();
+		DAOfertas dao=new DAOfertas(_nombreArchivo);
+		
+		for(Oferta oferta:dao.obtenerOfertas()){
+			aux.agregarOferta(oferta);
+		}
+		
+		return aux;
+	}
+	
 	ArrayList<Oferta> ordenarOfertas(Instancia instancia) {
 		ArrayList<Oferta> ofertasOrdenadas=instancia.obtenerOfertas();//clon de una instancia
 		Collections.sort(ofertasOrdenadas,_comparador);//ordena de < a >
